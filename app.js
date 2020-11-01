b8176515d5391419ba25ca3571c7961031e534c4
Sanad <s_wj_88@hotmail.com>
2020-11-02 01:47:04 +0200

    Add projects to the portifolio

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 const tl = gsap.timeline({ defaultes: { ease: "power1.out"}});
 
 tl.to(".firstg", { y: "0%", duration: 2 });
+
+
+gsap.to(".box1" ,{ scrollTrigger: ".box1", x: "0%", duration: 3});
+
