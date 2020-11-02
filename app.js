f1490c371b26eabf45b6b0d1082aa9714eb0a34e
Sanad <s_wj_88@hotmail.com>
2020-11-02 02:16:32 +0200

    Add javascript

--- app.js
+++ app.js
@@ -4,4 +4,9 @@ tl.to(".firstg", { y: "0%", duration: 2 });
 
 
 gsap.to(".box1" ,{ scrollTrigger: ".box1", x: "0%", duration: 3});
+gsap.to(".box2" ,{ scrollTrigger: ".box1", x: "0%", duration: 4});
+gsap.to(".box3" ,{ scrollTrigger: ".box1", x: "0%", duration: 2});
+gsap.to(".box4" ,{ scrollTrigger: ".box1", x: "0%", duration: 7});
+gsap.to(".box5" ,{ scrollTrigger: ".box1", x: "0%", duration: 8});
+gsap.to(".box6" ,{ scrollTrigger: ".box1", x: "0%", duration: 5});
 
